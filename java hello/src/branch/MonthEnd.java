948a25d96c00793788f2d0bc96a9d5b020d3c7e8
hb2003 <hb2003@hb2003-PC>
2016-06-09 17:13:38 +0900

    월말 구하는 클래스 작성 완료

---
+++ java hello/src/branch/MonthEnd.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package branch;
+
+import java.util.Scanner;
+
+import javax.swing.Spring;
+
+/**
+ * @date : 2016. 6. 9.
+ * @author : 박승주
+ * @file : MonthEnd.java
+ * @story :
+ */
+public class MonthEnd {
+	/**
+	 *  월을 입력하면 말일이 몇일인지 알려주는
+	 *  프로그램.
+	 *  단, 2월은 29일로 한정함
+	 *  1~12를 벗어난 숫자를 입력하면
+	 *  잘못된 입력값입니다 라고 뜬다.
+	 * */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int month = 0 , monthResult = 0;
+		String mon = "";
+		
+		System.out.println("월을 입력하세요");
+		
+	}
+}
